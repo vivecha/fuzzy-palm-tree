07bb674a1fb7e8361757f6846be50766ba3bcc19
vivecha <shabanovaliza21@gmail.com>
2025-05-13 17:48:42 +0300

    Update диплом.sql

--- диплом.sql
+++ диплом.sql
@@ -2,134 +2,147 @@ CREATE DATABASE IF NOT EXISTS MedicalSystem;
 
 USE MedicalSystem;
 
-CREATE TABLE IF NOT EXISTS Roles (
-    role_id INT AUTO_INCREMENT PRIMARY KEY,
-    role_name VARCHAR(255) NOT NULL UNIQUE
-);
+CREATE TABLE `appointments` (
+  `appointment_id` int NOT NULL AUTO_INCREMENT,
+  `patient_id` int NOT NULL,
+  `doctor_id` int NOT NULL,
+  `schedule_id` int DEFAULT NULL,
+  `appointment_date` datetime NOT NULL,
+  `complaints` text,
+  `diagnosis` text,
+  `treatment` text,
+  PRIMARY KEY (`appointment_id`),
+  KEY `doctor_id` (`doctor_id`),
+  KEY `schedule_id` (`schedule_id`),
+  KEY `idx_appointments_patient_id` (`patient_id`),
+  CONSTRAINT `appointments_ibfk_1` FOREIGN KEY (`patient_id`) REFERENCES `patients` (`patient_id`),
+  CONSTRAINT `appointments_ibfk_2` FOREIGN KEY (`doctor_id`) REFERENCES `doctors` (`doctor_id`),
+  CONSTRAINT `appointments_ibfk_3` FOREIGN KEY (`schedule_id`) REFERENCES `doctorschedules` (`schedule_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+CREATE TABLE `doctors` (
+  `doctor_id` int NOT NULL,
+  `specialty_id` int NOT NULL,
+  `user_id` int DEFAULT NULL,
+  PRIMARY KEY (`doctor_id`),
+  KEY `user_id` (`user_id`),
+  KEY `idx_doctors_specialty_id` (`specialty_id`),
+  CONSTRAINT `doctors_ibfk_1` FOREIGN KEY (`specialty_id`) REFERENCES `doctorspecialties` (`specialty_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+CREATE TABLE `doctorschedules` (
+  `schedule_id` int NOT NULL AUTO_INCREMENT,
+  `doctor_id` int NOT NULL,
+  `work_date` date NOT NULL,
+  `start_time` time NOT NULL,
+  `end_time` time NOT NULL,
+  PRIMARY KEY (`schedule_id`),
+  KEY `doctor_id` (`doctor_id`),
+  CONSTRAINT `doctorschedules_ibfk_1` FOREIGN KEY (`doctor_id`) REFERENCES `doctors` (`doctor_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE IF NOT EXISTS Positions (
-    position_id INT AUTO_INCREMENT PRIMARY KEY,
-    position_name VARCHAR(255) NOT NULL,
-    description TEXT NULL
-);
+CREATE TABLE `doctorspecialties` (
+  `specialty_id` int NOT NULL AUTO_INCREMENT,
+  `specialty_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`specialty_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE IF NOT EXISTS Employees (
-    employee_id INT AUTO_INCREMENT PRIMARY KEY,
-    position_id INT NOT NULL,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    middle_name VARCHAR(255) NULL,
-    date_of_birth DATE NULL,
-    gender ENUM('Male', 'Female') NULL,
-    phone_number VARCHAR(20) NULL,
-    email VARCHAR(255) NULL,
-    hire_date DATE NOT NULL,
-    fire_date DATE NULL,
-    FOREIGN KEY (position_id) REFERENCES Positions (position_id)
-);
+CREATE TABLE `employees` (
+  `employee_id` int NOT NULL AUTO_INCREMENT,
+  `position_id` int NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `middle_name` varchar(255) DEFAULT NULL,
+  `date_of_birth` date NOT NULL,
+  `gender` enum('Male','Female','Other') NOT NULL,
+  `phone_number` varchar(20) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `hire_date` date NOT NULL,
+  `fire_date` date DEFAULT NULL,
+  PRIMARY KEY (`employee_id`),
+  KEY `position_id` (`position_id`),
+  KEY `idx_employees_last_name` (`last_name`),
+  CONSTRAINT `employees_ibfk_1` FOREIGN KEY (`position_id`) REFERENCES `positions` (`position_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE IF NOT EXISTS Users (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
-    employee_id INT NULL, -- Связь с сотрудником (если это сотрудник больницы)
-    role_id INT NOT NULL,
-    username VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL, -- Хранить хешированный пароль
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    middle_name VARCHAR(255) NULL,
-    email VARCHAR(255) NULL,
-    FOREIGN KEY (role_id) REFERENCES Roles (role_id),
-    FOREIGN KEY (employee_id) REFERENCES Employees (employee_id) -- Может быть NULL, если это внешний пользователь
-);
+CREATE TABLE `medications` (
+  `medication_id` int NOT NULL AUTO_INCREMENT,
+  `medication_name` varchar(255) NOT NULL,
+  `description` text,
+  PRIMARY KEY (`medication_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE IF NOT EXISTS DoctorSpecialties (
-    specialty_id INT AUTO_INCREMENT PRIMARY KEY,
-    specialty_name VARCHAR(255) NOT NULL
-);
+CREATE TABLE `patients` (
+  `patient_id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `middle_name` varchar(255) DEFAULT NULL,
+  `date_of_birth` date NOT NULL,
+  `gender` enum('Male','Female','Other') NOT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `phone_number` varchar(20) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`patient_id`),
+  KEY `idx_patients_last_name` (`last_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE IF NOT EXISTS Doctors (
-    doctor_id INT PRIMARY KEY REFERENCES Employees (employee_id), -- Врачи - это сотрудники
-    specialty_id INT NOT NULL,
-    user_id INT NULL, -- Связь с таблицей Users
-    FOREIGN KEY (specialty_id) REFERENCES DoctorSpecialties (specialty_id),
-    FOREIGN KEY (user_id) REFERENCES Users (user_id)
-);
+CREATE TABLE `positions` (
+  `position_id` int NOT NULL AUTO_INCREMENT,
+  `position_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`position_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE IF NOT EXISTS Patients (
-    patient_id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    middle_name VARCHAR(255) NULL,
-    date_of_birth DATE NOT NULL,
-    gender ENUM('Male', 'Female') NOT NULL,
-    address VARCHAR(255) NULL,
-    phone_number VARCHAR(20) NULL,
-    email VARCHAR(255) NULL,
-    medical_history TEXT NULL
-);
+CREATE TABLE `roles` (
+  `role_id` int NOT NULL AUTO_INCREMENT,
+  `role_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`role_id`),
+  UNIQUE KEY `role_name` (`role_name`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE IF NOT EXISTS DoctorSchedules (
-    schedule_id INT AUTO_INCREMENT PRIMARY KEY,
-    doctor_id INT NOT NULL,
-    work_date DATE NOT NULL,
-    start_time TIME NOT NULL,
-    end_time TIME NOT NULL,
-    FOREIGN KEY (doctor_id) REFERENCES Doctors (doctor_id)
-);
+CREATE TABLE `prescriptions` (
+  `prescription_id` int NOT NULL AUTO_INCREMENT,
+  `appointment_id` int NOT NULL,
+  `medication_id` int NOT NULL,
+  `dosage` varchar(255) DEFAULT NULL,
+  `instructions` text,
+  PRIMARY KEY (`prescription_id`),
+  KEY `appointment_id` (`appointment_id`),
+  KEY `medication_id` (`medication_id`),
+  CONSTRAINT `prescriptions_ibfk_1` FOREIGN KEY (`appointment_id`) REFERENCES `appointments` (`appointment_id`),
+  CONSTRAINT `prescriptions_ibfk_2` FOREIGN KEY (`medication_id`) REFERENCES `medications` (`medication_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE IF NOT EXISTS Appointments (
-    appointment_id INT AUTO_INCREMENT PRIMARY KEY,
-    patient_id INT NOT NULL,
-    doctor_id INT NOT NULL,
-    schedule_id INT NULL, -- Ссылка на расписание, если прием запланирован
-    appointment_date DATETIME NOT NULL,
-    complaints TEXT NULL,
-    diagnosis TEXT NULL,
-    treatment TEXT NULL,
-    FOREIGN KEY (patient_id) REFERENCES Patients (patient_id),
-    FOREIGN KEY (doctor_id) REFERENCES Doctors (doctor_id),
-    FOREIGN KEY (schedule_id) REFERENCES DoctorSchedules (schedule_id)
-);
+CREATE TABLE `sickleaves` (
+  `sick_leave_id` int NOT NULL AUTO_INCREMENT,
+  `patient_id` int NOT NULL,
+  `doctor_id` int NOT NULL,
+  `appointment_id` int DEFAULT NULL,
+  `issue_date` date NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `diagnosis` text,
+  `notes` text,
+  `closed_date` date DEFAULT NULL,
+  PRIMARY KEY (`sick_leave_id`),
+  KEY `doctor_id` (`doctor_id`),
+  KEY `appointment_id` (`appointment_id`),
+  KEY `idx_sick_leaves_patient_id` (`patient_id`),
+  CONSTRAINT `sickleaves_ibfk_1` FOREIGN KEY (`patient_id`) REFERENCES `patients` (`patient_id`),
+  CONSTRAINT `sickleaves_ibfk_2` FOREIGN KEY (`doctor_id`) REFERENCES `doctors` (`doctor_id`),
+  CONSTRAINT `sickleaves_ibfk_3` FOREIGN KEY (`appointment_id`) REFERENCES `appointments` (`appointment_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE IF NOT EXISTS SickLeaves (
-    sick_leave_id INT AUTO_INCREMENT PRIMARY KEY,
-    patient_id INT NOT NULL,
-    doctor_id INT NOT NULL,
-    appointment_id INT NULL,  -- Ссылка на прием, на котором был выдан больничный
-    issue_date DATE NOT NULL,
-    start_date DATE NOT NULL,
-    end_date DATE NOT NULL,
-    diagnosis TEXT NULL,
-    notes TEXT NULL,
-    closed_date DATE NULL, -- Дата закрытия больничного
-    FOREIGN KEY (patient_id) REFERENCES Patients (patient_id),
-    FOREIGN KEY (doctor_id) REFERENCES Doctors (doctor_id),
-    FOREIGN KEY (appointment_id) REFERENCES Appointments (appointment_id)
-);
-
-CREATE TABLE IF NOT EXISTS Medications (
-    medication_id INT AUTO_INCREMENT PRIMARY KEY,
-    medication_name VARCHAR(255) NOT NULL,
-    description TEXT NULL
-);
-
-CREATE TABLE IF NOT EXISTS Prescriptions (
-    prescription_id INT AUTO_INCREMENT PRIMARY KEY,
-    appointment_id INT NOT NULL,
-    medication_id INT NOT NULL,
-    dosage VARCHAR(255) NULL,
-    instructions TEXT NULL,
-    FOREIGN KEY (appointment_id) REFERENCES Appointments (appointment_id),
-    FOREIGN KEY (medication_id) REFERENCES Medications (medication_id)
-);
-
-
-CREATE INDEX idx_patients_last_name ON Patients (last_name);
-CREATE INDEX idx_appointments_patient_id ON Appointments (patient_id);
-CREATE INDEX idx_sick_leaves_patient_id ON SickLeaves (patient_id);
-CREATE INDEX idx_doctors_specialty_id ON Doctors (specialty_id);
-CREATE INDEX idx_employees_last_name ON Employees (last_name);
-CREATE INDEX idx_users_username ON Users (username); 
-
+CREATE TABLE `users` (
+  `user_id` int NOT NULL AUTO_INCREMENT,
+  `employee_id` int DEFAULT NULL,
+  `role_id` int NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `username` (`username`),
+  KEY `role_id` (`role_id`),
+  KEY `employee_id` (`employee_id`),
+  KEY `idx_users_username` (`username`),
+  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`role_id`),
+  CONSTRAINT `users_ibfk_2` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`employee_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
